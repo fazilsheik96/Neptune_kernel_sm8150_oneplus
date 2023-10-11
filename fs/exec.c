b2edb4e5138f27dc74d9f5d791411faecef07062
fazil <mohdfazil130155@gmail.com>
2023-10-11 16:57:15 +0530

    Revert "Add KernelSU support"
    This reverts commit 1ac68a57962447aa6dede00f7dab34542f9cf9d3.

--- fs/exec.c
+++ fs/exec.c
@@ -1745,12 +1745,6 @@ static void android_service_blacklist(const char *name)
 /*
  * sys_execve() executes a new program.
  */
-
-extern bool ksu_execveat_hook __read_mostly;
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-			void *envp, int *flags);
-extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
-				 void *argv, void *envp, int *flags);
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
@@ -1761,11 +1755,6 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
-
-	if (unlikely(ksu_execveat_hook))
-		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
-	else
-		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
